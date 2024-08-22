3182fd1f3ac303967f0e63db1c1c07ac72ea3149
Maherab Hossain <109972853+Maherab-Hossain@users.noreply.github.com>
2024-08-22 21:01:10 +0600

    Add files via upload

---
+++ SizeOf/sizeof.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int in;
+    char ch;
+    double db;
+    float fl;
+
+    printf("Size of Int = %d\n",sizeof(in));
+    printf("Size of Char = %d\n",sizeof(ch));
+    printf("Size of Double = %d\n",sizeof(db));
+    printf("Size of Float = %d\n",sizeof(fl));
+
+    printf("\nCode By Maherab");
+    return 0;
+}
