ff66919c6e63b1a6dfababa77f012b35d6015943
Maherab Hossain <109972853+Maherab-Hossain@users.noreply.github.com>
2024-08-23 20:57:51 +0600

    Add files via upload

---
+++ Char to ASCII/char to ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+char uservalue;
+printf("Enter any Character: ");
+scanf("%c",&uservalue);
+printf("\nThe ASCII Value is:%d\n",uservalue);
+
+printf("\nCode By Maherab");
+return 0;
+}
