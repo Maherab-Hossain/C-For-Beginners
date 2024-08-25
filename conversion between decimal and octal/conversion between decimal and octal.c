736d68f6e8a4e3c92c2ac8d61d31d14bf251fe20
Maherab Hossain <109972853+Maherab-Hossain@users.noreply.github.com>
2024-08-25 15:31:35 +0600

    Add files via upload

---
+++ conversion between decimal and octal/conversion between decimal and octal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main (){
+    int user_number;
+
+    printf("Decimal Number: ");
+    scanf("%d",&user_number);
+
+    printf("Octal Number: %o \n",user_number);
+
+    printf("\nCode By Maherab");
+    return 0;
+}
