32fcea3eb3e9157730e3e19cc359dc309825b731
Maherab Hossain <109972853+Maherab-Hossain@users.noreply.github.com>
2024-08-25 15:31:56 +0600

    Add files via upload

---
+++ INT . LONG - MAX . MIN/INT - LONG MAX MIN.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main (){
+printf("The max int is %d\n",INT_MAX);
+printf("The min int is %d\n",INT_MIN);
+
+printf("The max int is %d\n",LONG_MAX);
+printf("The min int is %d\n",LONG_MIN);
+
+printf("\nCode By Maherab");
+return 0;
+}
