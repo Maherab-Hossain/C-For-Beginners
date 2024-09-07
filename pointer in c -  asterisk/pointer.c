214ac2954815b50d75874a5502e1fece1fd40f4c
Maherab Hossain <109972853+Maherab-Hossain@users.noreply.github.com>
2024-09-08 05:29:11 +0600

    Add files via upload

---
+++ pointer in c -  asterisk/pointer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    int m=14;
+    int *pointer;
+    pointer=&m;
+    printf("%d\n",m);
+    printf("%d\n",&m);
+    printf("%d\n",pointer);
+    printf("%d\n",*pointer);
+    printf("%d\n",&pointer);
+    return 0;
+}
